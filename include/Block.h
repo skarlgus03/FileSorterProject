bc0b97dad72af0768c315ac46ce19bea77bcba60
Rkillzsd <chogeulring@gmail.com>
2025-05-18 16:51:39 +0900

    Add files via upload

---
+++ include/Block.h
@@ -0,0 +1,36 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include <string>
+#include <vector>
+#include <memory>
+
+enum class FilterType {
+    EXTENSION,
+    KEYWORD,
+    DATE,
+    EXCEPTION
+};
+
+class Block {
+public:
+    Block(FilterType filterType,
+          const std::string& condition,
+          const std::string& movePath = "");
+
+    FilterType getFilterType() const;
+    const std::string& getCondition() const;
+    const std::string& getMovePath() const;
+    bool isLeaf() const;
+
+    void addChild(const std::shared_ptr<Block>& child);
+    const std::vector<std::shared_ptr<Block>>& getChildren() const;
+
+private:
+    FilterType filterType;
+    std::string condition;
+    std::string movePath;
+    std::vector<std::shared_ptr<Block>> children;
+};
+
+#endif // BLOCK_H

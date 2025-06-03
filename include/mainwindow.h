310befeaf3c55ad1a977eb54a3e2e2a7f9cec82f
lsj3042 <163094565+lsj3042@users.noreply.github.com>
2025-06-03 19:24:00 +0900

    Update mainwindow.h

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -3,25 +3,25 @@
 
 #include <QMainWindow>
 
+class FileDropWidget;
+class SettingsWidget;
+class FileViewWidget;
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class FileDropWidget;
-class SettingsWidget;
-class FileViewWidget;
-class QStackedWidget;
-
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
+
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
-    Ui::MainWindow    *ui;
-    QStackedWidget    *stack;
-    FileDropWidget    *fileDropWidget;
-    SettingsWidget    *settingsWidget;
-    FileViewWidget    *fileViewWidget;
+    Ui::MainWindow *ui;
+    FileDropWidget *fileDropWidget;
+    SettingsWidget *settingsWidget;
+    FileViewWidget *fileViewWidget;
 };

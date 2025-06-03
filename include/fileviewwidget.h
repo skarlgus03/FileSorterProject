3a92ed2a7f6b0b47b14d3ad722555aa9adedffdc
lsj3042 <163094565+lsj3042@users.noreply.github.com>
2025-06-03 19:23:20 +0900

    Update fileviewwidget.h

--- include/fileviewwidget.h
+++ include/fileviewwidget.h
@@ -2,8 +2,23 @@
 
 #include <QWidget>
 
+class QListWidget;
+class QListWidgetItem;
+
 class FileViewWidget : public QWidget {
     Q_OBJECT
 public:
     explicit FileViewWidget(QWidget *parent = nullptr);
+
+signals:
+    void fileSelected(const QString &filePath);
+
+public slots:
+    void addFilePath(const QString &filePath);      //경로받아 리스트에 추가
+
+private slots:
+    void onItemDoubleClicked(QListWidgetItem *item);        //ui에 표시되게
+
+private:
+    QListWidget *previewList;
 };

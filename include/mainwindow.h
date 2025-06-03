019fc7e6842a180e61fb7a91983abce8c5f0c82c
lsj3042 <163094565+lsj3042@users.noreply.github.com>
2025-06-03 22:26:32 +0900

    Update mainwindow.h

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -1,9 +1,27 @@
-﻿#pragma once
+#pragma once
+
 #include <QMainWindow>
 
-class MainWindow : public QMainWindow {
+class FileDropWidget;
+class SettingsWidget;
+class FileViewWidget;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
-    MainWindow(QWidget* parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;  // Designer .ui를 쓰지 않으면 nullptr 유지
+
+    FileDropWidget *fileDropWidget;
+    SettingsWidget *settingsWidget;
+    FileViewWidget *fileViewWidget;
 };

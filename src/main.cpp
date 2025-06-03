326f2111568744789897562099129a62d106ef0c
lsj3042 <163094565+lsj3042@users.noreply.github.com>
2025-06-03 19:31:50 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    return app.exec();
+    return a.exec();
 }
